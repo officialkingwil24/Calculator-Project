c43111e3021e66842b0e06ca1c006a183b6ab5da
Wilson PLouis <wilsonpierrrlouis14@gmail.com>
2020-10-09 14:07:34 -0400

    changes

--- script.js
+++ script.js
@@ -36,43 +36,84 @@ function inputChange(event){
 
     } else if(char === '='){
       // creating an else if statement for the variable to strictly equal to the equal button
-        myEval() // my function
-        screen.innerText = eval(screen.innerText)
+        myEval(screen.innerText)
+        // my function eval will solve any function inside the screen inner text
+        console.log(screen.innerText)
+        // inside the screen text will be equal to eval that will solve the problem inside
+        // the screen text to show the answer
         return;
+        // return the answer
     }  
-  // // change the calculator screen's inner html
   screen.innerText += char;
+  // change the calculator screen inner text adds the value of the right operand to a variable
+  // and assigns the result to the variable char
   
 }
 
 function myEval(str){
-  var solve = str.match(/[^0-9 \.]/g);
+  // created a function for my eval that is next to the parameter of string
+  var solve = str.match(/[^0-9\. ]/g);
+  // created a variable for solve so it can equal to str that was in the parameter
+  // of my eval and match it to my rejects it is used to search a string, for a match, 
+  // against a regular expression, it will return an Array object then the match is found
 
   console.log(solve)
+  // console.log my local variable
   var n = str.split(solve)
+  // created a variable of n as my number and equal to a string that will split a string into an 
+  // array of my local variable solve, and returns the new array
   var firstN = Number(n[0])
+  // created a variable of firstN as my first number to set it equal to a number that is my constructor
+  // and set a value as n that is my local variable and 0 in the bracket as my first number
   var secN = Number(n[1])
+  // created a variable of secN as my second number to set it equal to a number that is my constructor
+  // and set a value as n that is my local variable as well and 1 in the bracket as my second number
 
   if(solve == '+'){
+    // created a else if statement solve is equality equal to my plus button
+    // once you click it to convert the variable
     console.log(n[0][1]);
+    // console.log a method once you click my button to have my local variable n as my number
+    // to have the variable of my first number and second number inside the bracket
     screen.innerText = firstN + secN
+    // inside my screen text will be equal to my first number and add the second
     return;
+    // return the answer when you click equal
   }else if(solve == '-'){
+    // created a else if statement solve is equality equal to my subtract button
+    // once you click it to convert the variable
     console.log(n[0][1]);
+    // console.log a method once you click my button to have my local variable n as my number
+    // to have the variable of my first number and second number inside the bracket
     screen.innerText = firstN - secN
+    // inside my screen text will be equal to my first number and subtracting the second number
     return;
+    // return the answer when you click equal
   }else if(solve == '÷'){
+    // created a else if statement solve is equality equal to my divide button
+    // once you click it to convert the variable
     console.log(n[0][1]);
+    // console.log a method once you click my button to have my local variable n as my number
+    // to have the variable of my first number and second number inside the bracket
     screen.innerText = firstN / secN
+    // inside my screen text will be equal to my first number and divide the second number
     return;
+    // return the answer when you click equal
   }else if(solve == 'x'){
+    // created a else if statement solve is equality equal to my multitply button
+    // once you click it to convert the variable
     console.log(n[0][1]);
+    // console.log a method once you click my button to have my local variable n as my number
+    // to have the variable of my first number and second number inside the bracket
     screen.innerText = firstN * secN
+    // inside my screen text will be equal to my first number and multiply the second number
     return;
+    // return the answer when you click equal
   }
 }
 
 function addAllEventListeners(arr){
+  // created a function for all buttons inside the parameter, my array
 
   for(var i = 0; i < arr.length; i++){
       // iterate over the array
@@ -82,3 +123,5 @@ function addAllEventListeners(arr){
   }
  }
 addAllEventListeners(allButtons);
+// assigns all buttons to function and be able to click it
+// and show up inside the screen text
